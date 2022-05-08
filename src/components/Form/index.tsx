27b56b033600444efec9d9700bb1998cc77532d7
flpoliver <flpeolivier@gmail.com>
2022-05-08 20:18:57 -0300

    finish project

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,22 +1,82 @@
 import { ArrowLeft } from "phosphor-react-native";
-import React from "react";
-import { View, TextInput, Image, TouchableOpacity, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { theme } from "../../theme";
 import { feedbackTypes } from "../../utils/feedbackTypes";
+import { Button } from "../Button";
+import { ScreenshotButton } from "../ScreenshotButton";
 import { FeedbackType } from "../Widget";
+import * as FileSystem from "expo-file-system";
+
+import { captureScreen } from "react-native-view-shot";
 
 import { styles } from "./styles";
+import { api } from "../../libs/api";
 
 interface FormProps {
   feedbackType: FeedbackType;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
 }
 
-export function Form({ feedbackType }: FormProps) {
+export function Form({
+  feedbackType,
+  onFeedbackCanceled,
+  onFeedbackSent,
+}: FormProps) {
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleScreenshot() {
+    captureScreen({
+      format: "jpg",
+      quality: 0.8,
+    })
+      .then((uri) => setScreenshot(uri))
+      .catch((error) => console.log(error));
+  }
+
+  function handleScreenshotRemove() {
+    setScreenshot(null);
+  }
+
+  async function handleSendFeedback() {
+    if (isSendingFeedback) return;
+
+    setIsSendingFeedback(true);
+    const screenshotBase64 =
+      screenshot &&
+      (await FileSystem.readAsStringAsync(screenshot, { encoding: "base64" }));
+
+    try {
+      await api.post("feedbacks", {
+        type: feedbackType,
+        screenshot: `data:image/png;base64, ${screenshotBase64}`,
+        comment,
+      });
+
+      onFeedbackSent();
+    } catch (error) {
+      console.log(error);
+      setIsSendingFeedback(false);
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={onFeedbackCanceled}>
           <ArrowLeft
             size={24}
             weight="bold"
@@ -35,7 +95,19 @@ export function Form({ feedbackType }: FormProps) {
         style={styles.input}
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
+        autoCorrect={false}
+        onChangeText={setComment}
       />
-    </View>
+
+      <View style={styles.footer}>
+        <ScreenshotButton
+          onTakeShot={handleScreenshot}
+          onRemoveShot={handleScreenshotRemove}
+          screenshot={screenshot}
+        />
+
+        <Button isLoading={isSendingFeedback} onPress={handleSendFeedback} />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
